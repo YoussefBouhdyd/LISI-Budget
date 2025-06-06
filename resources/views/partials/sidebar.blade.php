260d386cebf5fd602ea2865b7baa0f903c05b2bd
YoussefBouhdyd <bouhdyd2004@gmail.com>
2025-06-06 12:20:40 +0100

    Refactor sidebar navigation for improved readability and maintainability

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,12 +2,42 @@
     <h1 class="logo txt-c p-relative">LISI</h1>
     <nav>
         <ul class="link">
-            <li class="rad-6 tt-capital"><a href="{{ url('/admin') }}"><i class="fa-regular fa-chart-bar fa-fw"></i><span class="hide-mobile">Dashboard</span></a></li>
-            <li class="active rad-6 tt-capital"><a href="{{ url('/admin-engagement') }}"><i class="fa-solid fa-file-signature fa-fw"></i><span class="hide-mobile">Engagements</span></a></li>
-            <li class="rad-6 tt-capital"><a href="{{ url('/transmitter') }}"><i class="fa-solid fa-paper-plane fa-fw"></i><span class="hide-mobile">Émetteur</span></a></li>
-            <li class="rad-6 tt-capital"><a href="{{ url('/budget') }}"><i class="fa-solid fa-wallet fa-fw"></i><span class="hide-mobile">Budget</span></a></li>
-            <li class="rad-6 tt-capital"><a href="setting.html"><i class="fa-solid fa-gear fa-fw"></i><span class="hide-mobile">Paramètre</span></a></li>
-            <li class="rad-6 tt-capital"><a href="profile.html"><i class="fa-regular fa-user fa-fw"></i><span class="hide-mobile">Profil</span></a></li>
+            <li class="rad-6 tt-capital">
+                <a href="{{ url('/admin') }}" class="{{ request()->is('admin') ? 'active' : '' }}">
+                    <i class="fa-regular fa-chart-bar fa-fw"></i>
+                    <span class="hide-mobile">Dashboard</span>
+                </a>
+            </li>
+            <li class="rad-6 tt-capital">
+                <a href="{{ url('/admin-engagement') }}" class="{{ request()->is('admin-engagement*') ? 'active' : '' }}">
+                    <i class="fa-solid fa-file-signature fa-fw"></i>
+                    <span class="hide-mobile">Engagements</span>
+                </a>
+            </li>
+            <li class="rad-6 tt-capital">
+                <a href="{{ url('/transmitter') }}" class="{{ request()->is('transmitter*') ? 'active' : '' }}">
+                    <i class="fa-solid fa-paper-plane fa-fw"></i>
+                    <span class="hide-mobile">Émetteur</span>
+                </a>
+            </li>
+            <li class="rad-6 tt-capital">
+                <a href="{{ url('/budget') }}" class="{{ request()->is('budget*') ? 'active' : '' }}">
+                    <i class="fa-solid fa-wallet fa-fw"></i>
+                    <span class="hide-mobile">Budget</span>
+                </a>
+            </li>
+            <li class="rad-6 tt-capital">
+                <a href="setting.html" class="{{ request()->is('setting') ? 'active' : '' }}">
+                    <i class="fa-solid fa-gear fa-fw"></i>
+                    <span class="hide-mobile">Paramètre</span>
+                </a>
+            </li>
+            <li class="rad-6 tt-capital">
+                <a href="profile.html" class="{{ request()->is('profile') ? 'active' : '' }}">
+                    <i class="fa-regular fa-user fa-fw"></i>
+                    <span class="hide-mobile">Profil</span>
+                </a>
+            </li>
         </ul>
     </nav>
 </div>
