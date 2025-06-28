0c0ee7b47f22333f0c2d9993254ce32ccd45f933
YoussefBouhdyd <bouhdyd2004@gmail.com>
2025-06-28 18:51:48 +0100

    feat: make the header change according the user role

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,40 +1,12 @@
-<div class="header d-flex p-20 flex-end align-c bg-white p-relative">
-    <div class="profile flex-between">
-        <button id="notification-btn" class="notification-btn">
-            <i class="fa-regular fa-bell fa-lg mr-20 p-relative">
-                <span class="notification-dot"></span>
-            </i>
-        </button>
-        <div id="notification-box" class="notification-box">
-            <div class="notification-header">
-                Notifications
-            </div>
-            <div class="notification-content" style="padding:0;">
-                <ul class="notification-list" id="notification-list">
-                    <li>
-                        <span class="notif-title">Nouvelle demande d'engagement</span>
-                        <span class="notif-time">il y a 2 min</span>
-                        <span class="notif-eye" title="Marquer comme lu">
-                            <i class="fa-solid fa-eye"></i>
-                        </span>
-                    </li>
-                    <li>
-                        <span class="notif-title">Budget mis à jour</span>
-                        <span class="notif-time">il y a 10 min</span>
-                        <span class="notif-eye" title="Marquer comme lu">
-                            <i class="fa-solid fa-eye"></i>
-                        </span>
-                    </li>
-                    <li>
-                        <span class="notif-title">Profil modifié avec succès</span>
-                        <span class="notif-time">il y a 1 heure</span>
-                        <span class="notif-eye" title="Marquer comme lu">
-                            <i class="fa-solid fa-eye"></i>
-                        </span>
-                    </li>
-                </ul>
-            </div>
+<div class="header d-flex p-20 s-between align-c bg-white p-relative">
+    @if (Auth::user()->role === 'admin')
+            <h2>Admin</h3>
+    @elseif (Auth::user()->role === 'user')
+        <div class="p-10 fit-content bg-blue rad-6 fw-bold c-white">
+            {{ Auth::user()->budget }} MAD
         </div>
+    @endif
+    <div class="profile flex-between">
         <button id="profile-btn" class="profile-btn profile-btn-style pointer">
             <img src="{{ asset('/imgs/user-profile.svg') }}" alt="user profile" class="rad-half">
         </button>
